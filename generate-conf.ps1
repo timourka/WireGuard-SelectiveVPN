ad158bf4e71c112bb06fde4aa31ac29edd6a3385
Timourka <timourkot@gmail.com>
2025-09-02 12:52:47 +0400

    запуск от имени администратора

--- generate-conf.ps1
+++ generate-conf.ps1
@@ -1,4 +1,6 @@
-﻿# Список доменов
+﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+# Список доменов
 $domainList = Get-Content ".\domains.txt"
 $ipList = @()
 
